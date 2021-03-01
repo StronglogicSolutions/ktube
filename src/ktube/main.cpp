1e6582b423424814fe534b5d64f879d9d3ef7c4d
logicp <logicp@logicp.ca>
2021-03-01 16:25:42 -0500

    modified example program

--- src/ktube/main.cpp
+++ src/ktube/main.cpp
@@ -5,14 +5,32 @@ int main(int argc, char** argv)
 {
 
   ktube::YouTubeDataAPI api{};
+  conversation::NLP     nlp{api.GetUsername()};
 
   api.init();
-
-  bool has_auth = api.is_authenticated();
   api.FetchLiveVideoID();
   api.FetchLiveDetails();
   api.FetchChatMessages();
-  api.PostMessage("Hello");
+  api.PostMessage("Hi");
+
+  if (api.HasChats()) {
+    api.ParseTokens();
+
+    ktube::LiveMessages messages = api.FindMentions();
+
+    if (!messages.empty())
+      ktube::log("Bot was mentioned");
+
+    for (const auto& message : messages)
+      for (const auto& token : message.tokens)
+        nlp.Insert(
+          conversation::Message{.text = message.text, .received = false},
+          message.author,
+          token.value);
+  }
+
+  for (const auto& conv : nlp.GetConversations())
+    ktube::log(conv.second->objective->toString());
 
   return 0;
 }
