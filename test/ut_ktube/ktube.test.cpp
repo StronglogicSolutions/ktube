834a50181a79959949e318f4b60d2e604bcebf9c
logicp <logicp@logicp.ca>
2021-03-08 20:50:24 -0500

    adding a test

--- test/ut_ktube/ktube.test.cpp
+++ test/ut_ktube/ktube.test.cpp
@@ -1,5 +1,14 @@
 #include "ktube.test.hpp"
 
-TEST(KTubeTest, DISABLED_Instantiate) {
+TEST(KTubeTest, FetchCommentThreadsTest) {
+  const std::string TEST_VIDEO_ID{"6_IGHMSsdD0"};
 
+  ktube::YouTubeDataAPI api{};
+
+  api.init();
+
+  std::vector<ktube::Comment> comments = api.FetchVideoComments(TEST_VIDEO_ID);
+
+
+  EXPECT_FALSE(comments.empty());
 }
