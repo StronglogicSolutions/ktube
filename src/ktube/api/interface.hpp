5dbc12a688a0cd89362d7b90f7680a8e3eb8ac0b
logicp <logicp@logicp.ca>
2021-03-10 01:30:12 -0500

    distinguishing between posting comments and replying to a comment

--- src/ktube/api/interface.hpp
+++ src/ktube/api/interface.hpp
@@ -15,25 +15,25 @@ class VideoAPI {
 public:
 virtual ~VideoAPI() {}
 
-virtual std::vector<VideoInfo>   get_videos() = 0;
+virtual std::vector<Video>       get_videos() = 0;
 virtual bool                     has_videos() = 0;
 virtual bool                     fetch_channel_data() = 0;
 virtual std::vector<ChannelInfo> fetch_channel_info(std::string id_string) = 0;
 virtual bool                     fetch_channel_videos() = 0;
 virtual std::vector<VideoStats>  fetch_video_stats(std::string id_string) = 0;
 virtual std::vector<ChannelInfo> fetch_youtube_stats() = 0;
-virtual std::vector<VideoInfo>   fetch_rival_videos(VideoInfo video) = 0;
-virtual std::vector<ChannelInfo> find_similar_videos(VideoInfo video) = 0;
+virtual std::vector<Video>       fetch_rival_videos(Video video) = 0;
+virtual std::vector<ChannelInfo> find_similar_videos(Video video) = 0;
 virtual std::vector<GoogleTrend> fetch_google_trends(std::vector<std::string> terms) = 0;
 virtual std::vector<TermInfo>    fetch_term_info(std::vector<std::string> terms) = 0;
-virtual std::vector<VideoInfo>   fetch_videos_by_terms(std::vector<std::string> terms) = 0;
+virtual std::vector<Video>       fetch_videos_by_terms(std::vector<std::string> terms) = 0;
 };
 
 class CommentAPI {
 public:
 virtual ~CommentAPI() {}
 virtual std::vector<Comment>     FetchVideoComments(const std::string& id) = 0;
-// virtual bool                     PostCommentReply(const Comment& comment) = 0;
+virtual bool                     PostComment(const Comment& comment) = 0;
 virtual bool                     PostCommentReply(const Comment& comment) = 0;
 };
 
