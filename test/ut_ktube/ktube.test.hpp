f69d04d75c926db6ab5e8eda93f6033d69c59872
logicp <logicp@logicp.ca>
2021-03-09 20:44:26 -0500

    added a test for replying to comments

--- test/ut_ktube/ktube.test.hpp
+++ test/ut_ktube/ktube.test.hpp
@@ -1,3 +1,8 @@
 #pragma once
 #include "gtest/gtest.h"
 #include "ktube/ktube.hpp"
+
+namespace ktube {
+const std::string TEST_VIDEO_ID{"y_vKxqz2uF8"};
+const std::string TEST_COMMENT_ID{"UgzEsIvaRJS64Rrs-bl4AaABAg"};
+} // ns ktube
