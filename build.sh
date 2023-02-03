395976535d40efdbf968bf9ad980e6ec11abd3f8
logicp <logicp@logicp.ca>
2023-02-03 00:32:34 -0500

    commenting these out

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 third_party/gn/out/gn gen out
 ninja -C out
 
-patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/build/lib/libcpr.so out/ktube
-patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/build/lib/libcpr.so out/libktube_lib.so
-patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/build/lib/libcpr.so out/ut_ktube
+# patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/build/lib/libcpr.so out/ktube
+# patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/build/lib/libcpr.so out/libktube_lib.so
+# patchelf --replace-needed libcpr.so.1 "$PWD"/third_party/cpr/build/lib/libcpr.so out/ut_ktube
